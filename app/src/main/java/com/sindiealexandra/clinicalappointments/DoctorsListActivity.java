70cce63d05184ec3fe2390e953642eba2223cf36
Alexandra Sindie <alexandra.sindie@gmail.com>
2022-03-17 16:41:39 +0200

    Added DoctorsListActivity

--- app/src/main/java/com/sindiealexandra/clinicalappointments/DoctorsListActivity.java
+++ app/src/main/java/com/sindiealexandra/clinicalappointments/DoctorsListActivity.java
@@ -1,14 +1,126 @@
 package com.sindiealexandra.clinicalappointments;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.sindiealexandra.clinicalappointments.adapters.DoctorRecyclerAdapter;
+import com.sindiealexandra.clinicalappointments.models.Doctor;
+import com.sindiealexandra.clinicalappointments.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DoctorsListActivity extends AppCompatActivity {
+
+    private static final String TAG = "Doctors List Activity";
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+    private Toolbar mToolbar;
+    private ProgressBar mProgressBar;
+    private RecyclerView mRecyclerView;
+    private DoctorRecyclerAdapter mDoctorRecyclerAdapter;
+    private List<Doctor> mDoctors;
+    private List<String> mDoctorIDs;
+    private String mSpecialization;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctors_list);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+        mToolbar = findViewById(R.id.toolbar);
+        mProgressBar = findViewById(R.id.progressBar);
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mDoctors = new ArrayList<>();
+        mDoctorIDs = new ArrayList<>();
+
+        Intent intent = getIntent();
+        mSpecialization = intent.getStringExtra("SPECIALIZATION");
+
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mDoctorRecyclerAdapter = new DoctorRecyclerAdapter(mDoctors);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mDoctorRecyclerAdapter);
+
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        // Configure Toolbar
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getString(R.string.available_doctors));
+        }
+
+        mDoctors = new ArrayList<>();
+        mDoctorIDs = new ArrayList<>();
+
+        mFirestore.collection("Users").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // Load users
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    if (document.getBoolean("admin") == null) {
+                        mDoctors.add(document.toObject(Doctor.class));
+                        mDoctorIDs.add(document.getId());
+                    }
+                }
+                mDoctorRecyclerAdapter.updateDoctors(mDoctors, mDoctorIDs);
+                mProgressBar.setVisibility(View.INVISIBLE);
+            } else {
+                Log.d(TAG, "Error getting documents: ", task.getException());
+            }
+        });
+    }
+
+    // Inflate toolbar menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.general_menu, menu);
+        return true;
+    }
+    // When the user clicks a button in the toolbar menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            // Go to main screen
+            case R.id.mainButton:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            // Start the Account Activity
+            case R.id.accountButton:
+                intent = new Intent(this, AccountActivity.class);
+                startActivity(intent);
+                return true;
+            // Log out user
+            case R.id.logoutButton:
+                mAuth.signOut();
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
---
+++ app/src/main/java/com/sindiealexandra/clinicalappointments/adapters/DoctorRecyclerAdapter.java
@@ -0,0 +1,87 @@
+package com.sindiealexandra.clinicalappointments.adapters;
+
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.sindiealexandra.clinicalappointments.R;
+import com.sindiealexandra.clinicalappointments.models.Doctor;
+
+import java.util.Date;
+import java.util.List;
+
+public class DoctorRecyclerAdapter extends RecyclerView.Adapter<DoctorRecyclerAdapter.ViewHolder> {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+    private FirebaseUser mFirebaseUser;
+
+    private TextView mNameTextView;
+    private TextView mPhoneTextView;
+
+    private List<Doctor> mDoctors;
+    private List<String> mDoctorIDs;
+
+    private static final String TAG = "User Recycler Adapter";
+
+    public DoctorRecyclerAdapter(List<Doctor> doctors) {
+        mDoctors = doctors;
+    }
+
+    @NonNull
+    @Override
+    public DoctorRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.doctors_item, parent, false);
+        return new DoctorRecyclerAdapter.ViewHolder(view);
+    }
+
+    // Fill card with data from Firestore
+    @Override
+    public void onBindViewHolder(@NonNull final DoctorRecyclerAdapter.ViewHolder holder, final int position) {
+        // Fill cards with data
+        String firstName = mDoctors.get(position).getFirstName();
+        String lastName = mDoctors.get(position).getLastName();
+        mNameTextView.setText(String.format("%s %s", firstName, lastName));
+
+        String phone = mDoctors.get(position).getPhone();
+        mPhoneTextView.setText(phone);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDoctors.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+
+            mAuth = FirebaseAuth.getInstance();
+            mFirestore = FirebaseFirestore.getInstance();
+            mFirebaseUser = mAuth.getCurrentUser();
+            mNameTextView = itemView.findViewById(R.id.nameTextView);
+            mPhoneTextView = itemView.findViewById(R.id.phoneTextView);
+        }
+    }
+
+    // Refresh fragment when something changes in the Recycler view
+    public void updateDoctors(List<Doctor> doctors, List<String> doctorIDs) {
+        mDoctors = doctors;
+        mDoctorIDs = doctorIDs;
+        notifyDataSetChanged();
+    }
+}
