41fc288282449ee250d739301fd520076ed58e6b
Alexandra Sindie <alexandra.sindie@gmail.com>
2022-04-01 14:00:00 +0300

    Added Date and Time Pickers

--- app/src/main/java/com/sindiealexandra/clinicalappointments/DoctorDetailsActivity.java
+++ app/src/main/java/com/sindiealexandra/clinicalappointments/DoctorDetailsActivity.java
@@ -1,26 +1,31 @@
 package com.sindiealexandra.clinicalappointments;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Switch;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.sindiealexandra.clinicalappointments.models.Doctor;
 
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.TimeZone;
 
 public class DoctorDetailsActivity extends AppCompatActivity {
 
@@ -28,7 +33,7 @@ public class DoctorDetailsActivity extends AppCompatActivity {
     private FirebaseFirestore mFirestore;
     private Toolbar mToolbar;
     private ProgressBar mProgressBar;
-    private TextView mNameTextView;
+    private TextView mSpecializationTextView;
     private Button mMakeAppointmentButton;
     private String mDoctorID;
     private Doctor mDoctor;
@@ -43,7 +48,7 @@ public class DoctorDetailsActivity extends AppCompatActivity {
 
         mProgressBar = findViewById(R.id.progressBar);
 
-        mNameTextView = findViewById(R.id.nameTextView);
+        mSpecializationTextView = findViewById(R.id.specializationTextView);
         mMakeAppointmentButton = findViewById(R.id.makeAppointmentButton);
 
         Intent intent = getIntent();
@@ -55,17 +60,45 @@ public class DoctorDetailsActivity extends AppCompatActivity {
         // Load Doctor from Firestore
         mFirestore.collection("Users").document(mDoctorID).get().addOnSuccessListener(documentSnapshot -> {
             mDoctor = documentSnapshot.toObject(Doctor.class);
-            mNameTextView.setText(Objects.requireNonNull(mDoctor).getFirstName());
+            mSpecializationTextView.setText(Objects.requireNonNull(mDoctor).getSpecialization());
 
             // Configure Toolbar
             setSupportActionBar(mToolbar);
             if (getSupportActionBar() != null) {
-                getSupportActionBar().setTitle(Objects.requireNonNull(mDoctor).getFirstName());
+                getSupportActionBar().setTitle(String.format("%s %s", Objects.requireNonNull(mDoctor).getFirstName(), Objects.requireNonNull(mDoctor).getLastName()));
             }
         });
 
+        // Get appointment date and time;
+        mMakeAppointmentButton.setOnClickListener(view -> {
+            // Show date picker
+            MaterialDatePicker<Long> datePicker =
+                    MaterialDatePicker.Builder.datePicker()
+                            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                            .build();
+            datePicker.show(getSupportFragmentManager(), "datePicker");
 
+            // On date set
+            datePicker.addOnPositiveButtonClickListener(dateSelection -> {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(dateSelection);
 
+                // Show time picker
+                MaterialTimePicker timePicker =
+                        new MaterialTimePicker.Builder()
+                                .setTimeFormat(TimeFormat.CLOCK_24H)
+                                .setHour(12)
+                                .setMinute(10)
+                                .build();
+                timePicker.show(getSupportFragmentManager(), "timePicker");
+                // On time set
+                timePicker.addOnPositiveButtonClickListener(dialog -> {
+                    calendar.set(Calendar.HOUR, timePicker.getHour());
+                    calendar.set(Calendar.MINUTE, timePicker.getMinute());
+                    Log.e(TAG, calendar.getTime().toString());
+                });
+            });
+        });
     }
 
     // Inflate toolbar menu
@@ -102,5 +135,4 @@ public class DoctorDetailsActivity extends AppCompatActivity {
                 return false;
         }
     }
-
 }
