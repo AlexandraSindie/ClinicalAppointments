eaabc9c725af5ea41e78060b6e31c7ac5ede27f7
Alexandra Sindie <alexandra.sindie@gmail.com>
2022-05-05 16:51:15 +0300

    Added Edit

--- app/src/main/java/com/sindiealexandra/clinicalappointments/AppointmentActivity.java
+++ app/src/main/java/com/sindiealexandra/clinicalappointments/AppointmentActivity.java
@@ -1,24 +1,37 @@
 package com.sindiealexandra.clinicalappointments;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.sindiealexandra.clinicalappointments.models.Appointment;
 import com.sindiealexandra.clinicalappointments.models.User;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
+@RequiresApi(api = Build.VERSION_CODES.P)
 public class AppointmentActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
@@ -30,6 +43,8 @@ public class AppointmentActivity extends AppCompatActivity {
     private String mAppointmentID;
     private String mUserType;
     private Appointment mAppointment;
+    private Button mEditAppointmentButton;
+    private Button mCancelAppointmentButton;
     private static final String TAG = "Appointment Activity";
 
     @Override
@@ -41,6 +56,8 @@ public class AppointmentActivity extends AppCompatActivity {
         mProgressBar = findViewById(R.id.progressBar);
         mUserTextView = findViewById(R.id.userTextView);
         mDateTextView = findViewById(R.id.dateTextView);
+        mEditAppointmentButton = findViewById(R.id.editAppointmentButton);
+        mCancelAppointmentButton = findViewById(R.id.cancelAppointmentButton);
 
         Intent intent = getIntent();
         mAppointmentID = intent.getStringExtra("APPOINTMENT_ID");
@@ -73,6 +90,45 @@ public class AppointmentActivity extends AppCompatActivity {
             if (getSupportActionBar() != null) {
                 getSupportActionBar().setTitle(Objects.requireNonNull(mAppointment).getSpecialization());
             }
+        });
+
+        // When user clicks the Edit Button
+        mEditAppointmentButton.setOnClickListener(view -> {
+            // Show date picker
+            MaterialDatePicker<Long> datePicker =
+                    MaterialDatePicker.Builder.datePicker()
+                            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                            .build();
+            datePicker.show(getSupportFragmentManager(), "datePicker");
+
+            // On date set
+            datePicker.addOnPositiveButtonClickListener(dateSelection -> {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(dateSelection);
+
+                // Show time picker
+                MaterialTimePicker timePicker =
+                        new MaterialTimePicker.Builder()
+                                .setTimeFormat(TimeFormat.CLOCK_24H)
+                                .setHour(12)
+                                .setMinute(10)
+                                .build();
+                timePicker.show(getSupportFragmentManager(), "timePicker");
+                // On time set
+                timePicker.addOnPositiveButtonClickListener(dialog -> {
+                    calendar.set(Calendar.HOUR, timePicker.getHour());
+                    calendar.set(Calendar.MINUTE, timePicker.getMinute());
+                    // Send data to Firestore
+                    editAppointment(calendar.getTime());
+                });
+            });
+        });
+
+        // When user clicks the Cancel Button
+        mCancelAppointmentButton.setOnClickListener(view -> {
+//            Intent intent = new Intent(SpecializationsActivity.this, DoctorsActivity.class);
+//            intent.putExtra("SPECIALIZATION","CARDIOLOGY");
+//            startActivity(intent);
         });
     }
 
@@ -109,5 +165,27 @@ public class AppointmentActivity extends AppCompatActivity {
             default:
                 return false;
         }
+    }
+
+    // Add user to Firestore
+    public void editAppointment(final Date date) {
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        // Create new appointment
+        assert firebaseUser != null;
+
+        mFirestore.collection("Appointments").document(mAppointmentID)
+                .update("date", date)
+                .addOnSuccessListener(aVoid -> {
+                    Log.d(TAG, "DocumentSnapshot successfully updated!");
+                    Toast.makeText(this, getString(R.string.appointment_edited), Toast.LENGTH_LONG).show();
+
+                })
+                .addOnFailureListener(e -> Log.w(TAG, "Error updating document", e));
+
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        // Start Main Activity
+        Intent intent = new Intent(getApplicationContext(), AppointmentsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/sindiealexandra/clinicalappointments/MainActivity.java
+++ app/src/main/java/com/sindiealexandra/clinicalappointments/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Magnifier;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
